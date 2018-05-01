1643f3985f549df80835386ac401f03c09231cee
badhon017 <badhonbrinta25@gmail.com>
2018-05-01 23:15:27 +0600

    3rd file 2nd commit

--- singleton.java
+++ singleton.java
@@ -7,3 +7,9 @@ interface iCoffee
             void cook();
         }
 		
+	class  CoffeeA implements iCoffee
+    {
+        public void cook () {
+                    System.out.println("Coffee A is made with milk, caffein and sugar");
+         }
+    }
